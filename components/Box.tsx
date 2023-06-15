93a6540daf4a7d954e4e440f324d2ca027c3f1d4
Springer <zxllks@yahoo.com>
2023-06-16 07:04:48 +0800

    layout 1 Sidebar

---
+++ components/Box.tsx
@@ -0,0 +1,20 @@
+import { twMerge } from 'tailwind-merge'
+
+interface BoxProps {
+  children: React.ReactNode
+  className?: string
+}
+
+const Box = (props: BoxProps) => {
+  const { children, className } = props
+
+  return (
+    <div
+      className={twMerge(`bg-neutral-900 rounded-lg h-fit w-full`, className)}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Box
